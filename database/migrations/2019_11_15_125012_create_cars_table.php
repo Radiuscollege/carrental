26f4d53a464ada622de15dfcb50fa4fe79a309a8
Fedde van Gils <feddevangils@gmail.com>
2019-11-22 12:54:53 +0100

    initial commit

---
+++ database/migrations/2019_11_15_125012_create_cars_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('categories_id');
+
+            $table->string('brand');
+            $table->string('model');
+            $table->string('color')->nullable();
+            $table->string('license_plate');
+            $table->timestamps();
+
+            $table->foreign('categories_id')
+                ->references('id')
+                ->on('categories');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
